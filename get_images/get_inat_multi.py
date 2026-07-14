92c5ecad918198ef862ec2839ca0e86eb5c7b5bb
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2026-07-14 16:54:07 +0100

    add comment about extending search as numbers per spp for GB low

--- get_images/get_inat_multi.py
+++ get_images/get_inat_multi.py
@@ -6,6 +6,21 @@ from pyinaturalist import get_observations
 
 # Great Britain place ID
 PLACE_ID = 6857
+# Note GB-only gives less than 1000 images for some species. Many images are
+# footprints, spraints, scats so unusable.
+# Could omit place_id to get more images, or use a bounding box for north
+# European countries etc by editing the get_observations() call below.
+#response = get_observations(
+#    taxon_name=taxon_name,
+#    photos=True,
+#    nelat=56.0,
+#    nelng=16.0,
+#    swlat=49.0,
+#    swlng=-11.0,
+#    quality_grade="research",
+#    per_page=PER_PAGE,#
+#    page=page
+#)
 
 # Taxa to download
 SPECIES = {
@@ -125,3 +140,12 @@ if __name__ == "__main__":
             future.result()
 
     print("All downloads complete.")
+    print("\nImage counts by folder:")
+
+    base_dir = Path("images/images_raw")
+
+
+    for folder in sorted(base_dir.iterdir()):
+        if folder.is_dir():
+            count = len(list(folder.glob("*.jpg")))
+            print(f"{folder.name}: {count} images")
