9e7ce391a0befa7e48a50844583ab3cbbc0a1a2a
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2026-07-15 14:29:12 +0100

    Can download stoat but no polecat records

--- pre-processing/download_lila_subset.py
+++ pre-processing/download_lila_subset.py
@@ -38,7 +38,7 @@ for s in lila_base_urls.values():
 
 # If any of these strings appear in the common name of a species, we'll download that image
 # species_of_interest = ['grey fox','gray fox','cape fox','red fox','kit fox']
-species_of_interest = ['bear']
+species_of_interest = ['stoat','european stoat','ermine','eurasian stoat', 'eurasian ermine']
 
 # We'll write images, metadata downloads, and temporary files here
 lila_local_base = os.path.expanduser('~/lila')
